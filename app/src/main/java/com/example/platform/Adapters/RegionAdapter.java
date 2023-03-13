d2019fbfb98da7414835a901b52246e067993b10
Sakii470 <mateuszek370@gmail.com>
2023-03-13 22:14:12 +0100

    Add animation to activity_main

--- app/src/main/java/com/example/platform/Adapters/RegionAdapter.java
+++ app/src/main/java/com/example/platform/Adapters/RegionAdapter.java
@@ -49,41 +49,11 @@ public class RegionAdapter extends ArrayAdapter<RegionModel> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
         View view = LayoutInflater.from(parent.getContext()).inflate((R.layout.region_list_item1), null, true);
-       // animation = AnimationUtils.loadAnimation(this.pokazPolaActivity,R.anim.animation1);
         TextView tvID = view.findViewById(R.id.textView);
         LinearLayout ll_bg = view.findViewById(R.id.ll_bg);
         int pos = position + 1;
 
-        //tvID.setAnimation(animation);
-        //int number = getRandom(1,8);
-//        int number=2;
-//        if(number == 1){
-//            ll_bg.setBackground(ContextCompat.getDrawable(pokazPolaActivity, R.drawable.gradient_1));
-//        }
-//        if(number == 2){
-//            ll_bg.setBackground(ContextCompat.getDrawable(pokazPolaActivity, R.drawable.gradient_2));
-//        }
-//        if(number == 3){
-//            ll_bg.setBackground(ContextCompat.getDrawable(pokazPolaActivity, R.drawable.gradient_3));
-//        }
-//        if(number == 4){
-//            ll_bg.setBackground(ContextCompat.getDrawable(pokazPolaActivity, R.drawable.gradient_4));
-//        }
-//        if(number == 5){
-//            ll_bg.setBackground(ContextCompat.getDrawable(pokazPolaActivity, R.drawable.gradient_5));
-//        }
-//        if(number == 6){
-//            ll_bg.setBackground(ContextCompat.getDrawable(pokazPolaActivity, R.drawable.gradient_6));
-//        }
-//        if(number == 7){
-//            ll_bg.setBackground(ContextCompat.getDrawable(pokazPolaActivity, R.drawable.gradient_7));
-//        }
-//        if(number == 8){
-//            ll_bg.setBackground(ContextCompat.getDrawable(pokazPolaActivity, R.drawable.gradient_8));
-//        }
-
         tvID.setText("Pole " + pos);
-      //  tvID.setAnimation(animation);
         i++;
 
         return view;
